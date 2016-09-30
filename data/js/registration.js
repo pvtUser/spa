1fab9a2927d55e4c8e0816d73008cd5f5e192c47
Eugene <evgenktulu@mail.ru>
2016-09-30 06:46:27 +0000

    registration and login forms added

---
+++ data/js/registration.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $('#registration_button').on('click', function() {
+        var user = new Backendless.User();
+        user.email = $('#email').val();
+        user.password = $('#password').val();
+        user.name = $('#firstName').val();
+
+        try {
+            Backendless.UserService.register(user);
+            $('#output').prepend('<div class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Вы успешно зарегистрировались!</strong></div>');
+            $('a#login').replaceWith('<a class="nav-link" id="logout" href="#logout">logout</a>');
+        }
+        catch (err) {
+            console.log("error message - " + err.message);
+            console.log("error code - " + err.statusCode);
+            $('#output').prepend('<div class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Проверьте правильность введенных данных!</strong></div>');
+        }
+    });
+});
